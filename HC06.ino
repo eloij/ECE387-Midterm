e3f2e664a12e98e930f90d45679d089d48fba6ba
eloij <eloij@miamioh.edu>
2016-03-11 22:00:10 -0500

    Added files via upload

---
+++ HC06.ino
@@ -0,0 +1,59 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(0, 1); // RX | TX
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+int sensorPin = A0;
+int sensorValue = 0;
+LiquidCrystal_I2C lcd(0x27,20,4);
+ 
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.println("Arduino with HC-06 is ready");
+ 
+    // HC-06 default baud rate is 9600
+    BTSerial.begin(9600);  
+    Serial.println("BTserial started at 9600");
+
+    lcd.init();                      // initialize the lcd 
+    lcd.init();
+    lcd.begin(20,4);
+    lcd.backlight();
+    lcd.setCursor(0,0);
+    lcd.println("Midterm Juliano");
+}
+ 
+void loop()
+{
+  // Keep reading from HC-06 and send to Arduino Serial Monitor
+  if (BTSerial.available())
+    sensorValue = BTSerial.read();
+    Serial.write(sensorValue);
+ 
+  // Keep reading from Arduino Serial Monitor and send to HC-06
+  if (Serial.available())
+    BTSerial.write(Serial.read());
+
+
+  //Print on LCD
+  if(sensorValue < 100){
+    lcd.setCursor(2,2);
+    lcd.print(0);
+    lcd.setCursor(3,2);
+    lcd.print(0);
+    lcd.setCursor(4,2);
+    lcd.print(sensorValue);
+  }
+  else if(sensorValue < 1000){
+    lcd.setCursor(2,2);
+    lcd.print(0);
+    lcd.setCursor(3,2);
+    lcd.print(sensorValue);
+  }
+  else{
+    lcd.setCursor(2,2);
+    lcd.print(sensorValue);
+  }
+}
---
+++ HC_05.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(0 , 1); // RX | TX
+// Connect the HC-05 TX to Arduino pin 0 RX. 
+// Connect the HC-05 RX to Arduino pin 1 TX
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+ 
+char c = ' ';
+ 
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.println("Arduino is ready");
+ 
+    // HC-05 default serial speed for AT mode is 38400
+    BTserial.begin(38400);  
+}
+ 
+void loop()
+{
+    sensorValue = analogRead(sensorPin);
+    // Keep reading from HC-05 and send to Arduino Serial Monitor
+    if (BTserial.available())
+    {
+        Serial.write(BTserial.read());
+    }
+      
+    // Keep reading from Arduino Serial Monitor and send to HC-05 (Communication Arduino-Bluetooth Module)
+    if (Serial.available())
+    {
+        Serial.write(sensorValue); 
+        BTserial.write(sensorValue);  //Send the message 
+    }
+}
+
